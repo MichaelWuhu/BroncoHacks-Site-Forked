81867fd6cee93a1c0288c149350f414f5e921997
tommy phao <thomasphao04@gmail.com>
2024-03-18 13:38:50 -0700

    - test imlpementation of prop components

--- Static-Site/src/App.jsx
+++ Static-Site/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 
+import Profile from'./Components/Profile.jsx';
+
 function App() {
 
   return (
     <>
       <div>Home/Starting Page</div>
+
+    {<Profile></Profile>}
+    
     </>
   )
 }
--- Static-Site/src/Components/Profile.jsx
+++ Static-Site/src/Components/Profile.jsx
@@ -1,10 +1,52 @@
-function Profile() {
+import "./Profile.css"; /* temporary just so it visually looks fine */
 
-    return (
-      <>
-        <div>Profile Component</div>
-      </>
-    )
-  }
-  
-  export default Profile
+function Member({ person }) {
+  return (
+    <div className="profile">
+      <img
+        className="profilepicture"
+        src={person.image}
+        alt={"Picture of" + person.name}
+      />
+      <div className="profilebody">
+        <name>{person.name}</name>
+        <major>{person.major}</major>
+        <etc>{person.role}</etc>
+        <etc>{person.year}</etc>
+        <etc>{person.link}</etc>
+      </div>
+    </div>
+  );
+}
+
+function ProfileProps() {
+  return (
+    <>
+      <Member
+        size={100}
+        person={{
+          name: "Tommy Phao",
+          major: "Business Administration - Computer Information Systems",
+          role: "test",
+          year: "Second year",
+          link: "https://github.com/xdkaine",
+          image:
+            "https://media.licdn.com/dms/image/D5603AQFI7ZBRCOheqQ/profile-displayphoto-shrink_200_200/0/1698549537540?e=1715817600&v=beta&t=ojbaGRCop6sU6VzK6VmFyc9n0dPMdnSQPrvw2E-iL_I",
+        }}
+      />
+    </>
+  );
+}
+
+function Profile() {
+  return (
+    <div className="container">
+      <h2>Meet the Team!</h2>
+      <div className="profilegrid">
+        <ProfileProps />
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
