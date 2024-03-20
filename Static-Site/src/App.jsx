8113daafb4c0f258f3c283c9b3e49dbb325c2c9b
Gerardo Solis <gsolis5114@gmail.com>
2024-03-20 06:46:31 -0700

    Updated NavBar.jsx

--- Static-Site/src/App.jsx
+++ Static-Site/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom' 
+import '../src/App.css'
+import NavBar from './Components/NavBar'
+import FAQ from './Pages/FAQ'
+import MeetTheTeam from './Pages/MeetTheTeam'
+import Sponsors from './Pages/Sponsors'
 
 function App() {
-
   return (
-    <>
-      <div>Home/Starting Page</div>
-    </>
-  )
+    <Router>
+      <div className="navbar">
+        <NavBar/>
+      </div>
+      <Routes>
+          <Route path="FAQ" element={<FAQ/>}/>
+          <Route path="MeetTheTeam" element={<MeetTheTeam/>}/>
+          <Route path="Sponsors" element={<Sponsors/>}/>
+      </Routes>
+    </Router>
+
+    //<>
+    //<div>Home/Starting Page</div>
+    //</>
+  );
 }
 
 export default App
