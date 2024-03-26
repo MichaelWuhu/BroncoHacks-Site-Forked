88fea3cff2af313db63d2a6eda3ce629632edcfa
Michael <mwu250@gmail.com>
2024-03-25 22:43:25 -0700

    added props and proptypes to components components change- countdown and profile minor changes to pages but these aren't really important

--- Static-Site/src/Components/Profile.jsx
+++ Static-Site/src/Components/Profile.jsx
@@ -1,6 +1,9 @@
 import "../styles/Profile.css"; /* temporary just so it visually looks fine */
+import PropTypes from "prop-types";
 
-function Profile({ person }) {
+function Profile(props) {
+const person = props.person;
+
   return (
     <div className="profile">
       <img
@@ -21,5 +24,14 @@ function Profile({ person }) {
     </div>
   );
 }
+
+Profile.propTypes = {
+  person: PropTypes.shape({
+    picture: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    role: PropTypes.string,
+    linkedin: PropTypes.string,
+  }),
+};
 
 export default Profile;
