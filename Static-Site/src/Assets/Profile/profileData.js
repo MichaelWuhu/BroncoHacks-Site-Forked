cb4a5dfe4bc15f2b03a5b0bc4928df4c59203091
arminerika <145848856+arminerika@users.noreply.github.com>
2024-04-02 10:31:39 -0700

    add meet the team page and style using profile component (#45)

---
+++ Static-Site/src/Assets/Profile/profileData.js
@@ -0,0 +1,29 @@
+const profileData = [
+    {
+      name: 'Tommy Phao',
+      role: 'Developer',
+      picture: '/src/Assets/Profile/team/tommy.jpg',  
+      linkedin: 'https://www.linkedin.com/in/tommmyphao',
+    },
+    {
+      name: 'Armin Erika Polanco',
+      role: 'Developer',
+      picture: '/src/Assets/Profile/team/arminerika.png',  
+      linkedin: 'https://www.linkedin.com/in/arminerika',
+    },
+    {
+        name: 'Rane Dy',
+        role: 'Developer',
+        picture: '/src/Assets/Profile/team/user.png',  
+        linkedin: 'https://www.linkedin.com/in/ranedy',
+    },
+    {
+        name: 'Team Member',
+        role: 'Role',
+        picture: '/src/Assets/Profile/team/user.png',  
+        linkedin: 'https://www.linkedin.com/in/teammember',
+    },
+    // Add more team members
+  ];
+  
+  export default profileData;
--- Static-Site/src/Pages/MeetTheTeam.jsx
+++ Static-Site/src/Pages/MeetTheTeam.jsx
@@ -1,10 +1,25 @@
-function App() {
+import '/src/styles/MeetTheTeam.css'; 
+import Profile from '/src/Components/Profile'; 
+import profileData from '/src/Assets/Profile/profileData'; 
 
-    return (
-      <>
-        <div>Meet The Team Page</div>
-      </>
-    )
-  }
-  
-  export default App
+function MeetTheTeam() {
+  return (
+    <div>
+      <div className="meet-the-team">
+        <h1>Meet the Team</h1>
+        <div className="team-container">
+          {/* Map through profileData to create bubbles for each team member */}
+          {profileData.map((person, index) => (
+            <div key={index} className="profile-wrapper">
+              <div className="profile-bubble">
+                <Profile person={person} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MeetTheTeam;
