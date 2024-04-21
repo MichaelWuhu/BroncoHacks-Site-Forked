7bfd2839b588bb206910e413e11c75b13d3a543d
Justin Nguyen <ju5t1n.nguy3n284@gmail.com>
2024-04-21 16:46:00 -0700

    commented out base line in vite config to fix image paths for now

--- Static-Site/src/Components/DescriptionBox.jsx
+++ Static-Site/src/Components/DescriptionBox.jsx
@@ -6,14 +6,14 @@ function DescriptionBox() {
       <>
         <div className="db-container">
           <div className="db-title-wrapper">
-            <img className="db-logo" src="../Static-Site/src/Assets/Profile/icon/BroncoHacks_Logo.png" alt="BroncoHacks Logo"/>
+            <img className="db-logo" src="../src/Assets/Profile/icon/BroncoHacks_Logo.png" alt="BroncoHacks Logo"/>
             <span className="db-title">
               <h1>BroncoHacks</h1>
               <h2>California Polytechnic State University, Pomona</h2>
             </span>
           </div>
           <div className="db-content-wrapper">
-            <img className="db-mascot" src="../Static-Site/src/Assets/DescriptionBox/imgs/cppBroncoMascot.png" alt="Bronco Mascot"/>
+            <img className="db-mascot" src="../src/Assets/DescriptionBox/imgs/cppBroncoMascot.png" alt="Bronco Mascot"/>
             <span className="db-filler"></span>
             <h4>A 24-Hour hackathon where you can showcase your skills in front of industry professionals!</h4>
           </div>
