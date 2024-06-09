569980c9bd385a92db35c6912b345080b1f40d55
rane20 <145800950+rane20@users.noreply.github.com>
2024-06-08 20:56:28 -0700

    FAQ Changes

--- Static-Site/src/Pages/FAQ.jsx
+++ Static-Site/src/Pages/FAQ.jsx
@@ -1,48 +1,100 @@
-
+import React from 'react';
 import '../styles/FAQ.css';
-
+import FAQBoxes from '../Components/FAQBoxes';
 
 function FAQ() {
-    return (
-        <div className="faq-container">
-            <div className="title-container">
-                <h1 className="faq-title">FAQ</h1>
-           </div>
-            
-            <div className="bubbled-rectangle">
-                <div className="inner-rectangle">
-                    <p>Do I need to have experience in programming?</p>
-                </div>
-                <div className="text-content">
-                    <p>While it does help, we take a lot of consideration and have lots of events that require no experience such as MiniHack.</p>
-                </div>
-            </div>
+  const faqs = [
+    {
+      question: "Do I Need to Have Experience in Programming?",
+      answer: "While it does help, we take a lot of consideration and have lots of events that require no experience such as MiniHack."
+    },
+    {
+      question: "Are the Hackathons Free of Entry?",
+      answer: "Yes! All events ran by BroncoHacks are free of charge to enter!"
+    },
+    {
+      question: "Can non-CPP Students Participate?",
+      answer: "Only for select Hackathons, like the one happening this upcoming Fall is open to all students!"
+    },
+    {
+      question: "What Categories Can We Compete In?",
+      answer: "The categories teams can choose from are Web development, Cybersecurity, and Data Science"
+    },
+    {
+      question: "What Are The Costs To Participate?",
+      answer: "Entry into the event as well as food will be completely free! There will be two meals of free food provided for each participant"
+    },
+    {
+      question: "How Will Teams Be Organized?",
+      answer: "Team sizes will range from 1-4 people and can be organized amongst themselves. Additionally, if you would like to join a team but have no one in mind, we will be providing a separate form where you can fill out and be placed into one!"
+    },
+    {
+      question: "Who Can Participate?",
+      answer: "We welcome both undergraduate and graduate students to participate"
+    },
+    {
+      question: "Can Students From Other Schools Participate?",
+      answer: "Only currently enrolled CPP students can register and compete"
+    },
+    {
+      question: "How Much Programming Experience Do I Need?",
+      answer: "We welcome students of all skill levels! The purpose of BroncoHacks is to provide first time and experienced programmers with opportunities to gain new skills, apply existing skills, and work in a team!"
+    },
+    {
+      question: "Will I Need To Stay There The Whole Time?",
+      answer: "You are not required to stay the full time. However, in order to qualify you must have at least one team member present on day when you present your project to the judges"
+    },
+    {
+      question: "Will There Be Accommodations For Overnight Stay?",
+      answer: "The BSC (Bronco Student Center) will be closed at 12 AM and will open up again at 8 AM. From 12 AM to 8 AM, all hackers who would like to continue working overnight on campus will be moved to the 24-hour computer lab"
+    },
+    {
+      question: "What Are The Prizes?",
+      answer: "TBD"
+    },
+    {
+      question: "What Should I Bring?",
+      answer: "TBD"
+    },
+  ];
 
+  // Split questions into left and right columns
+  const leftFaqs = faqs.slice(0, 7);
+  const rightFaqs = faqs.slice(7, 13);
 
-            <div className="bubbled-rectangle">
-                <div className="inner-rectangle">
-                    <p>Are the Hackathons free of entry?</p>
-                </div>
-                <div className="text-content">
-                    <p>Yes! All events ran by BroncoHacks are free of charge to enter!</p>
-                </div>
-            </div>
-
-           <div className="bubbled-rectangle">
-                <div className="inner-rectangle">
-                    <p>Can non-CPP students participate?</p>
-                </div>
-                <div className="text-content">
-                    <p>Only for select Hackathons, like the one happening this upcoming Fall is open to all students!</p>
-                </div>
-            </div>
-            
-
+  return (
+    <div className="faq-container">
+      <div className="faq-background">
+      <div className="title-container">
+        <h1 className="faq-title">FAQ</h1>
+      </div>
+      <div className="faq-grid">
+        <div className="faq-column">
+          {leftFaqs.map((faq, index) => (
+            <FAQBoxes
+              key={index}
+              question={faq.question}
+              answer={faq.answer}
+            />
+          ))}
         </div>
-    );
+        <div className="faq-column">
+          {rightFaqs.map((faq, index) => (
+            <FAQBoxes
+              key={index + 7} // Ensure keys are unique across both columns
+              question={faq.question}
+              answer={faq.answer}
+            />
+          ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FAQ;
+
 
 
 
