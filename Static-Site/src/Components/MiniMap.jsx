1c8c4b62ba8e610e7b56c09f800afacde5965e0b
Jairus <117532457+jai-rus@users.noreply.github.com>
2024-07-14 19:31:47 -0700

    Added minimap to homepage and styling (#77)
    * Added minimap to homepage and styling
    
    Added minimap to homepage and added a border around it. Added border around homepage slideshow
    
    * Minor css changes
    
    ---------
    
    Co-authored-by: chillwafflez <ju5t1n.nguy3n284@gmail.com>

--- Static-Site/src/Components/MiniMap.jsx
+++ Static-Site/src/Components/MiniMap.jsx
@@ -1,8 +1,39 @@
+"use client";
+
+import.meta.env.PROD
+
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  MapControl,
+  ControlPosition,
+} from "@vis.gl/react-google-maps";
+//https://visgl.github.io/react-google-maps/docs/api-reference/components/map
+
+//Maybe add the advanced marker pinn with the mapid?
+
 function MiniMap() {
+    const position = { lat: 34.05638771273931, lng: -117.82131090540534} //position of bronco student center
 
     return (
       <>
-        <div>Minimap Component</div>
+        <APIProvider apiKey={import.meta.env.VITE_REACT_MAP_KEY}> 
+          <div style={{height: "25vh", 
+                      width: "55vh",
+                      borderRadius: "1vh",
+                      overflow: 'hidden'}}>
+            <Map defaultZoom = {17} //inital zoom
+              defaultCenter = {position} //initally centers on bronco student center
+              //mapid here
+              disableDefaultUI={true} //turns off the UI so the map is less clustered
+            >
+              
+            </Map>
+          </div>
+        </APIProvider>
+
       </>
     )
   }
--- Static-Site/src/Pages/Home.jsx
+++ Static-Site/src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import { slidesData } from "../Assets/Profile/slidesData";
 import DescriptionBox from '../Components/DescriptionBox';
 import Countdown from '../Components/Countdown';
 import '/src/styles/Home.css'
+import { useState } from 'react'
+import Minimap from '../Components/MiniMap.jsx';
 
 function Home() {
 
@@ -11,13 +13,20 @@ function Home() {
         <div className="home-featured-container">
           <div className="home-featured-row1">
             <DescriptionBox/>
-            <div className="slides">
-              <div className="containerStyles">
-                <Slideshow slides={slidesData} parentWidth={500}/>
+            <div className="slide-wrapper">
+              <div className="slides">
+                <div className="containerStyles">
+                  <Slideshow slides={slidesData}/>
+                </div>
               </div>
             </div>
           </div>
           <Countdown deadline="October, 31, 2024" />
+          <div className="home-map">
+            <div className="map-wrapper">
+              {<Minimap></Minimap>}
+            </div>
+          </div>
         </div>
       </>
     )
