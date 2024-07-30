e6c824f3013cdeb4d7b42ce330cca48133c2050d
rane20 <145800950+rane20@users.noreply.github.com>
2024-07-30 16:15:14 -0700

    Created Error page and styled it according to figma design (#93)

--- Static-Site/src/App.jsx
+++ Static-Site/src/App.jsx
@@ -7,6 +7,7 @@ import MeetTheTeam from './Pages/MeetTheTeam'
 import Sponsors from './Pages/Sponsors'
 import AboutUs from './Pages/About.jsx'
 import Footer from './Components/Footer.jsx'
+import Error from './Pages/Error.jsx'
 import "./styles/Profile.css"; /* temporary just so it visually looks fine */
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path="/FAQ" element={<FAQ/>}/>
           <Route path="/MeetTheTeam" element={<MeetTheTeam/>}/>
           <Route path="/Sponsors" element={<Sponsors/>}/>
+          <Route path="/Error" element={<Error/>}/>
       </Routes>
       <div className="foot">
         <Footer/>
---
+++ Static-Site/src/Pages/Error.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import '../styles/Error.css';
+
+function Error() {
+    return (
+      <div className="error-container">
+          <div className="Etitle-container">
+            <h1 className="error-title">404</h1>
+            <div className="error-phrase">
+            <p>Page Not Found</p>
+            </div>
+          </div>
+        </div>
+    );
+  }
+  
+  export default Error;
