22b6516e629a6df15fb80ae9f4f8892d74bc9fa1
Michael <98852023+MichaelWuhu@users.noreply.github.com>
2024-09-07 19:36:10 -0700

    Users (#6)
    * fix: getUser (by id)
    
    * feat: create user working
    STILL NEED TO ADD IN PWORD HASHING (shouldn't be too hard)
    
    * feat: delete user route

--- server/queries/users.js
+++ server/queries/users.js
@@ -1,32 +1,39 @@
 const pool = require("../database");
 
 async function getUsers() {
-  const users = await pool.query('SELECT * FROM "User"');
+  const users = await pool.query(`SELECT * FROM "User"`);
   return users.rows;
 }
 
+// THIS IS POSTGRESQL SYNTAX THAT WORKS (it's a bit different from MySQL)
 async function getUser(id) {
-  const [user] = await pool.query(`SELECT * FROM User WHERE userid = ?`, [id]);
+  const result = await pool.query(`SELECT * FROM "User" WHERE userid = $1`, [
+    id,
+  ]);
+  user = result.rows[0];
   return user;
 }
 
 async function getUserByEmail(email) {
-  const [user] = await pool.query(`SELECT * FROM User WHERE email = ?`, [email]);
+  const result = await pool.query(`SELECT * FROM "User" WHERE email = $1`, [
+    email,
+  ]);
+  user = result.rows[0];
+  console.log(user);
   return user;
 }
 
-async function createAccount(email, password) {
-  const [result] = await pool.query(
-    `INSERT INTO User (email, password) VALUES (?, ?)`,
-    [email, password]
+async function createAccount(name, email, password) {
+  const result = await pool.query(
+    `INSERT INTO "User" (name, email, password) VALUES ($1, $2, $3)`,
+    [name, email, password]
   );
-  const id = result.insertId;
-  return getUser(id);
+  return getUserByEmail(email);
 }
 
 async function deleteUser(id) {
   const user = await getUser(id);
-  await pool.query(`DELETE FROM User WHERE userid = ?`, [id]);
+  await pool.query(`DELETE FROM "User" WHERE userid = $1`, [id]);
   return user;
 }
 
