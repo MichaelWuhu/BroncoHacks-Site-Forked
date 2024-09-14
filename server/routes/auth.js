24d18ff88dac585233823f1320e45451e44881cb
Michael <mwu250@gmail.com>
2024-09-14 13:22:37 -0700

    chore: add validation for auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/auth");
+const { accountLoginValidator } = require("../validators/auth");
 
 // POST route to login
-router.post("/login", AuthController.login);
+router.post("/login", accountLoginValidator, AuthController.login);
 
 module.exports = router;
---
+++ server/validators/auth.js
@@ -0,0 +1,26 @@
+const { param, body } = require("express-validator");
+
+const accountLoginValidator = [
+  // Validate email
+  body("email")
+    .notEmpty()
+    .withMessage("Email cannot be empty")
+    .isEmail()
+    .withMessage("Invalid email format"),
+  // Validate password
+  body("password")
+    .notEmpty()
+    .withMessage("Password cannot be empty")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(
+      /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()?])[A-Za-z\d!@#$%^&*()?]{8,}$/
+    )
+    .withMessage(
+      "Password must contain at least 1 uppercase letter, 1 number, and 1 special character"
+    ),
+];
+
+module.exports = {
+    accountLoginValidator
+};
